b116ad492ca71b304a5fdc76769fdf3969c962d9
Harry Han <jianqiaohan@gmail.com>
2021-05-18 11:15:44 -0400

    set up project

---
+++ src/test/java/com/itlize/group/may/MayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itlize.group.may;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
